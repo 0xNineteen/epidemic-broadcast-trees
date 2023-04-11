9ebd46b7e670354f7b2399e9a49aaeee77046027
0xNineteen <0x39015319@gmail.com>
2023-04-11 15:26:37 -0400

    client message broadcasts

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use std::time::Duration;
+use std::{time::{Duration, SystemTime}, collections::hash_map::DefaultHasher};
 
 use anyhow::Result;
+use sha2::Sha256;
+use sha2::Digest;
 use tokio::{sync::mpsc::{self, Sender, Receiver}, time::sleep};
-use tracing::{info, Instrument};
+use tracing::info;
 use rand::{self, Rng, thread_rng};
 
-pub const HASH_BYTE_SIZE: usize = 32;
+pub const HASH_BYTE_SIZE: usize = 4;
 pub type Sha256Bytes = [u8; HASH_BYTE_SIZE];
+pub type Node = (usize, Sender<Message>);
 
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone, Copy)]
 pub enum Message { 
@@ -25,19 +28,19 @@ pub struct ClientMessage {
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone, Copy)]
 pub struct BodyMessage { 
     pub data: u32, 
-    pub sender_id: u32,
+    pub sender_id: usize,
     pub msg_id: Sha256Bytes,
 }
 
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone, Copy)]
 pub struct HeaderMessage { 
-    pub sender_id: u32,
+    pub sender_id: usize,
     pub msg_id: Sha256Bytes,
 }
 
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone, Copy)]
 pub struct IDMessage { 
-    pub sender_id: u32,
+    pub sender_id: usize,
 }
 
 /* 
@@ -73,25 +76,79 @@ match message => {
     }
 }
 */
-pub async fn msg_reciever(id: usize, mut reciever: Receiver<Message>, world: Vec<Sender<Message>>) -> Result<()> { 
+pub async fn msg_reciever(id: usize, mut reciever: Receiver<Message>, peers: Vec<Node>) -> Result<()> { 
+    let fanout_size = 2;
+
+    let peer_ids = peers.iter().map(|(id, _)| *id).collect::<Vec<_>>();
+
     // track what messages it has recieved so far 
     let mut msg_ids = vec![]; 
     let mut msg_data = vec![];
 
-    // // track peers
-    // let mut eager_peers = vec![];
-    // let mut lazy_peers = vec![];
+    // track peers
+    // first eager
+    let mut eager_peers = vec![];
+    let n_nodes = peers.len();
+    let mut eager_ids = Vec::with_capacity(fanout_size);
+    for _ in 0..fanout_size { 
+        // sample without replacement 
+        loop { 
+            let node_index: usize = thread_rng().gen_range(0..n_nodes);
+            let node_id = peer_ids[node_index];
 
+            if !eager_ids.contains(&node_id) { 
+                eager_ids.push(node_id);
+                eager_peers.push(&peers[node_index]);
+                break;
+            }
+        }
+    }
+
+    // then lazy the rest
+    let lazy_peers = peers
+        .iter()
+        .filter(|(id, _)| !eager_ids.contains(id))
+        .collect::<Vec<_>>();
+
+    let lazy_ids = lazy_peers.iter().map(|(id, _)| *id).collect::<Vec<_>>();
+
+    info!("NODE {id:?}: lazy {:?} eager {:?} total_n {:?}", lazy_ids, eager_ids, peers.len());
+    assert!(lazy_peers.len() + eager_peers.len() == peers.len());
+
+    // start
     while let Some(msg) = reciever.recv().await { 
-        info!("recieved: {msg:?}");
+        info!("NODE {id:?}: recieved: {msg:?}");
 
         match msg { 
             Message::ClientMessage(msg) => { 
+                let time = std::time::SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_nanos();
+                let mut hasher = Sha256::new();
+
+                hasher.update(time.to_le_bytes());
+                hasher.update(msg.data.to_le_bytes());
+                let msg_id = hasher.finalize().as_slice()[..4].try_into().unwrap();
+
+                msg_ids.push(msg_id);
+                msg_data.push(msg.data);
+
+                let eager_message = Message::EagerMessage(
+                    BodyMessage { data: msg.data, sender_id: id, msg_id }
+                );
+                let lazy_message = Message::LazyMessage(
+                    HeaderMessage { sender_id: id, msg_id }
+                );
+
+                info!("NODE {id:?}: broadcasting...");
+
                 // broadcast to eager 
+                broadcast(&eager_peers, eager_message).await?;
                 // broadcast to lazy 
+                broadcast(&lazy_peers, lazy_message).await?;
+
             }
             Message::EagerMessage(msg) => {
                 if !msg_ids.contains(&msg.msg_id) { 
+                
 
                     // keep track 
                     msg_ids.push(msg.msg_id);
@@ -110,9 +167,8 @@ pub async fn msg_reciever(id: usize, mut reciever: Receiver<Message>, world: Vec
     Ok(())
 }
 
-pub async fn broadcast(peers: &Vec<Sender<Message>>, msg: Message) -> Result<()> { 
-    info!("sending: {msg:?}");
-    for peer in peers { 
+pub async fn broadcast(peers: &Vec<&Node>, msg: Message) -> Result<()> { 
+    for (_, peer) in peers { 
         peer.send(msg).await?; // todo: move await out of loop
     }
 
@@ -124,18 +180,19 @@ pub async fn main() {
     tracing_subscriber::fmt::init();
 
     // !!
-    let n_nodes = 2; 
+    let n_nodes = 4; 
 
     let world = (0..n_nodes).map(|_| mpsc::channel::<Message>(100)).collect::<Vec<_>>();
-    let world_senders = world.iter().map(|(sender, _)| sender.clone()).collect::<Vec<_>>();
+    // (id, channel)
+    let world_senders = world.iter().enumerate().map(|(i, (sender, _))| (i, sender.clone())).collect::<Vec<_>>();
 
     let mut i = 0;
     for (_, reciever) in world {
-        let mut peer_list = world_senders.clone();
-        peer_list.remove(i);
+        let mut peers = world_senders.clone();
+        peers.remove(i);
 
         let _handle = tokio::spawn(async move { 
-            msg_reciever(i, reciever, peer_list).await.unwrap();
+            msg_reciever(i, reciever, peers).await.unwrap();
         });
 
         i += 1;
@@ -148,7 +205,7 @@ pub async fn main() {
     loop { 
         let node_index: u64 = thread_rng().gen_range(0..n_nodes);
         let message = Message::ClientMessage(ClientMessage { data: step });
-        let sender = &world_senders[node_index as usize];
+        let (_, sender) = &world_senders[node_index as usize];
 
         info!("Client sending {step:?}...");
         sender.send(message).await.unwrap();
